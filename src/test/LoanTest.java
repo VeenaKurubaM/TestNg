cf115c2bcefb32f7447637383eef1943e159e5c6
Your Name <you@example.com>
2022-09-03 21:20:56 -0700

    updated Loan File

--- src/test/LoanTest.java
+++ src/test/LoanTest.java
@@ -1,40 +1,45 @@
 package test;
 
-
 import org.testng.annotations.Test;
 
-
-@Test(groups= {"regression"})
+@Test(groups = { "regression" })
 public class LoanTest extends BaseClass {
 
-
-	//@Test(groups= {"sanity"})
+	@Test(groups = { "sanity" }, enabled = true, description = "This wtest to validate home loan", priority = 1)
 	public void HomeLoanTest() {
 
 		System.out.println("Inside home loan test");
 
 	}
 
-	//@Test(groups= {"regression"})
+	@Test(groups = { "regression" }, priority = 3)
 	public void PersonalLoan() {
 
 		System.out.println("Inside personal loan test");
 	}
 
-	//@Test
+//Disable this specific method in running
+	@Test(enabled = false, priority = 2)
 	public void CarLoan() {
 
 		System.out.println("Inside Car Loan");
 
 	}
 
-	
-
-	@Test
+	@Test(priority = 0)
 	public void PersonalLoan1() {
 
 		System.out.println("Inside PersonalLoan1 ");
 
 	}
 
+//if we dont give any priority then it will first execute before priority is 0
+	// if no priority then tests will execute depending on the name of methods on
+	// ascending order
+	@Test
+	public void BusinessLoan() {
+
+		System.out.println("Inside Business loan ");
+
+	}
 }
